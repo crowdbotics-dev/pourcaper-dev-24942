3e8480cb2a6ef130240eaee62b59d52eb0ef7802
Crowdbotics <engineering@crowdbotics.com>
2022-06-20 09:03:36 +0000

    Initial commit.

---
+++ backend/pourcaper_dev_24942/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pourcaper_dev_24942 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pourcaper_dev_24942.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pourcaper_dev_24942",
+  url: "https://pourcaper_dev_24942.botics.co",
+  api: "https://pourcaper_dev_24942.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
